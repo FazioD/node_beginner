38e604cdb2c7237a203b5829701ee2dd195c4492
FazioD <faziod@uw.edu>
2016-05-11 13:48:04 -0700

    adding changes

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,5 @@
 var querystring = require('querystring');
-var  fs = require('fs');
+var fs = require('fs');
 var formidable = require('formidable');
 
 function start(response) {
@@ -11,7 +11,7 @@ function start(response) {
   'content="text/html"; charset=UTF-8 /> '+
   '</head>'+
   '<body>'+
-  '<form action="/upload" enctype="multipart/form-data"'
+  '<form action="/upload" enctype="multipart/form-data"';
   'method="post">'+
   '<input type="file" name="upload" multiple="multiple">' +
   '<input type="submit" value="Upload file" />' +
@@ -31,20 +31,22 @@ function upload(response, request) {
   var form = new formidable.IncomingForm();
   console.log('about to parse');
   form.parse(request, function(error, fields, files) {
-    console.log("parsing done");
-  }
+    console.log('parsing done');
 
-fs.rename(files.upload.path, "/tmp/test.png", function(error) {
-  if(error) {
-    fs.unlink('tmp/test.png');
-    fs.rename(files.upload.path, '/tmp/test.png');
-  }
-});
 
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('received image:<br/>');
-  response.write('<img src='/show' />')
-  response.end();
+    fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+      if(error) {
+        fs.unlink('tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('received image:<br/>');
+    response.write('<img src="/show"/>');
+    response.end();
+  });
+
 }
 
 function show(response) {
